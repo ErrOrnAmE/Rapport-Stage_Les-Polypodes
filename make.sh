3abacfb0ffd8af6f53fc5e5f38faef21a4e853fd
Thibaud Courtoison <do.not.press.enter@gmail.com>
2015-06-01 11:49:24 +0200

    Refact folder architecture

--- make.sh
+++ make.sh
@@ -1,1 +1,6 @@
-./clean.sh ; /usr/texbin/pdflatex -shell-escape rapport.tex ; /usr/texbin/pdflatex -shell-escape rapport.tex ; ./clean.sh
+cd sources/
+rm *~ *.backup *.aux *.toc *.blg *.log *.out *.bbl *.dvi *.nav *.pyg *.snm *.lof 2> /dev/null
+/usr/texbin/pdflatex -shell-escape rapport.tex
+/usr/texbin/pdflatex -shell-escape rapport.tex
+mv rapport.pdf ../
+rm *~ *.backup *.aux *.toc *.blg *.log *.out *.bbl *.dvi *.nav *.pyg *.snm *.lof 2> /dev/null
